c46fd6f0b9cae4c1e80cb06259331c425fe918aa
Radheshyam1234 <rdk7352669258@gmail.com>
2025-02-19 22:35:52 +0530

    fix

--- src/middlewares/express-session-middleware.js
+++ src/middlewares/express-session-middleware.js
@@ -19,8 +19,10 @@ const sessionMiddleware = session({
   store: sessionStore,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
+    // secure: process.env.NODE_ENV === "production",
+    // sameSite: "Strict",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   },
   genid: function () {
