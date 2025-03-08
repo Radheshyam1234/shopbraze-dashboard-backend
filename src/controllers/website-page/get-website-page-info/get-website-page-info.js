2573f1006f8e25033b694b2a279416fd4e65885f
Radheshyam1234 <rdk7352669258@gmail.com>
2025-03-08 10:02:24 +0530

    fix

--- src/controllers/website-page/get-website-page-info/get-website-page-info.js
+++ src/controllers/website-page/get-website-page-info/get-website-page-info.js
@@ -2,7 +2,7 @@ import { WebsitePage } from "../../../models/website-page/website-page.model.js"
 
 const getHomePageInfo = async (req, res) => {
   try {
-    const pageData = await WebsitePage.findOne({ type: "home_page" });
+    const pageData = await WebsitePage.findOne({ type: "home_page" }); // also add for which seller it is
     res.status(200).json({ data: pageData });
   } catch (error) {
     console.log(error);
